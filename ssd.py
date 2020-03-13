loss calculation bug fixed

ssd.py
@@ -20,14 +20,14 @@ class CustomLoss(Layer):
         conf_loss = confidence / class / label loss value
     """
 
-    def __init__(self, neg_pos_ratio, loc_loss_weight, **kwargs):
+    def __init__(self, neg_pos_ratio, loc_loss_alpha, **kwargs):
         super(CustomLoss, self).__init__(**kwargs)
         self.neg_pos_ratio = neg_pos_ratio
-        self.loc_loss_weight = loc_loss_weight
+        self.loc_loss_alpha = loc_loss_alpha
 
     def get_config(self):
         config = super(CustomLoss, self).get_config()
-        config.update({"neg_pos_ratio": self.neg_pos_ratio, "loc_loss_weight": self.loc_loss_weight})
+        config.update({"neg_pos_ratio": self.neg_pos_ratio, "loc_loss_alpha": self.loc_loss_alpha})
         return config
 
     def call(self, inputs):
@@ -38,8 +38,9 @@ class CustomLoss(Layer):
         #
         pos_cond = tf.reduce_any(tf.not_equal(actual_bbox_deltas, 0), axis=2)
         pos_bbox_indices = tf.where(pos_cond)
+        pos_bbox_count = tf.math.count_nonzero(pos_cond, axis=1)
         # Hard negative mining
-        neg_bbox_indices_count = tf.math.count_nonzero(pos_cond, axis=1) * int(self.neg_pos_ratio)
+        neg_bbox_indices_count = pos_bbox_count * int(self.neg_pos_ratio)
         pre_loss_fn = tf.losses.CategoricalCrossentropy(from_logits=True, reduction=tf.losses.Reduction.NONE)
         loss = pre_loss_fn(actual_labels, pred_labels)
         # Remove positive index values from negative calculations
@@ -52,18 +53,20 @@ class CustomLoss(Layer):
         neg_bbox_indices = tf.stack([tf.where(neg_cond)[:,0], sorted_loss[neg_cond]], 1)
         # Merge pos and neg indices for confidence loss calculation
         selected_indices = tf.concat([pos_bbox_indices, neg_bbox_indices], 0)
+        #
+        total_pos_bboxes = tf.cast(tf.maximum(tf.reduce_sum(pos_bbox_count), 1), tf.float32)
         # Localization / Bbox loss calculation
         y_true_bbox = tf.gather_nd(actual_bbox_deltas, pos_bbox_indices)
         y_pred_bbox = tf.gather_nd(pred_bbox_deltas, pos_bbox_indices)
-        loc_loss_fn = tf.losses.Huber()
-        loc_loss = loc_loss_fn(y_true_bbox, y_pred_bbox)
+        loc_loss_fn = tf.losses.Huber(reduction="sum")
+        loc_loss = loc_loss_fn(y_true_bbox, y_pred_bbox) * self.loc_loss_alpha
         # Confidence / Label loss calculation
         y_true_label = tf.gather_nd(actual_labels, selected_indices)
         y_pred_label = tf.gather_nd(pred_labels, selected_indices)
-        conf_loss_fn = tf.losses.CategoricalCrossentropy()
+        conf_loss_fn = tf.losses.CategoricalCrossentropy(reduction="sum")
         conf_loss = conf_loss_fn(y_true_label, y_pred_label)
         #
-        return loc_loss * self.loc_loss_weight, conf_loss
+        return loc_loss / total_pos_bboxes, conf_loss / total_pos_bboxes
 
 class HeadWrapper(Layer):
     """Merging all feature maps for detections.
@@ -114,11 +117,11 @@ class HeadWrapper(Layer):
         return tf.concat([conv4_3_reshaped, conv7_reshaped, conv8_2_reshaped,
                           conv9_2_reshaped, conv10_2_reshaped, conv11_2_reshaped], axis=1)
 
-def get_model(hyper_params, loc_loss_weight=10, mode="training"):
+def get_model(hyper_params, loc_loss_alpha=10, mode="training"):
     """Generating ssd model for hyper params.
     inputs:
         hyper_params = dictionary
-        loc_loss_weight = localization loss multiplier, to prevent the imbalanced loss calculation
+        loc_loss_alpha = localization loss multiplier, to prevent the imbalanced loss calculation
         mode = "training" or "inference"
 
     outputs:
@@ -197,16 +200,16 @@ def get_model(hyper_params, loc_loss_weight=10, mode="training"):
         actual_bbox_deltas = Input(shape=(None, 4), name="input_bbox_deltas", dtype=tf.float32)
         actual_labels = Input(shape=(None, hyper_params["total_labels"]), name="input_labels", dtype=tf.float32)
         #
-        loc_loss, conf_loss = CustomLoss(hyper_params["neg_pos_ratio"], loc_loss_weight, name="custom_loss_calculation")(
+        loc_loss, conf_loss = CustomLoss(hyper_params["neg_pos_ratio"], loc_loss_alpha, name="custom_loss_calculation")(
                                             [actual_bbox_deltas, actual_labels, pred_bbox_deltas, pred_labels])
         #
         ssd_model = Model(inputs=[input_img, actual_bbox_deltas, actual_labels],
                           outputs=[pred_bbox_deltas, pred_labels, loc_loss, conf_loss])
         #
         ssd_model.add_loss(loc_loss)
-        ssd_model.add_metric(loc_loss, name="ssd_loc_loss", aggregation="mean")
+        ssd_model.add_metric(loc_loss, name="loc_loss", aggregation="mean")
         ssd_model.add_loss(conf_loss)
-        ssd_model.add_metric(conf_loss, name="ssd_conf_loss", aggregation="mean")
+        ssd_model.add_metric(conf_loss, name="conf_loss", aggregation="mean")
     else:
         ssd_model = Model(inputs=input_img, outputs=[pred_bbox_deltas, pred_labels])
     return ssd_model