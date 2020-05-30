1597bf6ff616d0967ccf415a2d57cbfb62f6dbb6
Furkan OM <furkanomerustaoglu@gmail.com>
2020-05-30 19:27:39 +0300

    prediction process and custom image management updated

--- predictor.py
+++ predictor.py
@@ -20,17 +20,25 @@ else:
 hyper_params = train_utils.get_hyper_params(backbone)
 #
 test_data, info = data_utils.get_dataset("voc/2007", "test")
+total_items = data_utils.get_total_item_size(info, "test")
 labels = data_utils.get_labels(info)
 labels = ["bg"] + labels
 hyper_params["total_labels"] = len(labels)
 img_size = hyper_params["img_size"]
 
+data_types = data_utils.get_data_types()
+data_shapes = data_utils.get_data_shapes()
+padding_values = data_utils.get_padding_values()
+
 if use_custom_images:
-    test_data = data_utils.get_image_data_from_folder(custom_image_path, img_size, img_size)
+    img_paths = data_utils.get_custom_imgs(custom_image_path)
+    total_items = len(img_paths)
+    test_data = tf.data.Dataset.from_generator(lambda: data_utils.custom_data_generator(
+                                               img_paths, img_size, img_size), data_types, data_shapes)
 else:
     test_data = test_data.map(lambda x : data_utils.preprocessing(x, img_size, img_size))
-    padded_shapes, padding_values = data_utils.get_padded_batch_params()
-    test_data = test_data.padded_batch(batch_size, padded_shapes=padded_shapes, padding_values=padding_values)
+
+test_data = test_data.padded_batch(batch_size, padded_shapes=data_shapes, padding_values=padding_values)
 
 ssd_model = get_model(hyper_params)
 ssd_model_path = io_utils.get_model_path(backbone)
@@ -39,9 +47,7 @@ ssd_model.load_weights(ssd_model_path)
 prior_boxes = bbox_utils.generate_prior_boxes(hyper_params["feature_map_shapes"], hyper_params["aspect_ratios"])
 ssd_decoder_model = get_decoder_model(ssd_model, prior_boxes, hyper_params)
 
-for image_data in test_data:
-    imgs, _, _ = image_data
-    pred_bboxes, pred_labels, pred_scores = ssd_decoder_model.predict_on_batch(imgs)
-    for i, img in enumerate(imgs):
-        denormalized_bboxes = bbox_utils.denormalize_bboxes(pred_bboxes[i], img_size, img_size)
-        drawing_utils.draw_bboxes_with_labels(img, denormalized_bboxes, pred_labels[i], pred_scores[i], labels)
+step_size = train_utils.get_step_size(total_items, batch_size)
+pred_bboxes, pred_labels, pred_scores = ssd_decoder_model.predict(test_data, steps=step_size, verbose=1)
+
+drawing_utils.draw_predictions(test_data, pred_bboxes, pred_labels, pred_scores, labels, batch_size)
--- trainer.py
+++ trainer.py
@@ -42,9 +42,10 @@ img_size = hyper_params["img_size"]
 train_data = train_data.map(lambda x : data_utils.preprocessing(x, img_size, img_size, augmentation.apply))
 val_data = val_data.map(lambda x : data_utils.preprocessing(x, img_size, img_size))
 
-padded_shapes, padding_values = data_utils.get_padded_batch_params()
-train_data = train_data.shuffle(batch_size*4).padded_batch(batch_size, padded_shapes=padded_shapes, padding_values=padding_values)
-val_data = val_data.padded_batch(batch_size, padded_shapes=padded_shapes, padding_values=padding_values)
+data_shapes = data_utils.get_data_shapes()
+padding_values = data_utils.get_padding_values()
+train_data = train_data.shuffle(batch_size*4).padded_batch(batch_size, padded_shapes=data_shapes, padding_values=padding_values)
+val_data = val_data.padded_batch(batch_size, padded_shapes=data_shapes, padding_values=padding_values)
 #
 ssd_model = get_model(hyper_params)
 ssd_custom_losses = CustomLoss(hyper_params["neg_pos_ratio"], hyper_params["loc_loss_alpha"])
